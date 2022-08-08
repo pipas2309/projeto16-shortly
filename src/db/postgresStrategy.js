273a76d098ea647bf337c30ee2826f07a1672c24
Lucas Palharini <lucas230991@hotmail.com>
2022-08-08 00:46:05 -0300

    feat: database connection

---
+++ src/db/postgresStrategy.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import pg from "pg";
+
+const { Pool } = pg;
+
+//const connectionString = process.env.DATABASE_URL; //Alternative URL
+
+const connectionString = { 
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+}
+
+const connection = new Pool(connectionString);
+
+export default connection;
