7a6acb4611dcf78795a421ce06136d49acdc6c31
Lucas Palharini <lucas230991@hotmail.com>
2022-08-08 15:00:19 -0300

    Prepare for deploy

--- src/db/postgresStrategy.js
+++ src/db/postgresStrategy.js
@@ -3,19 +3,13 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-//const connectionString = process.env.DATABASE_URL; //Alternative URL
-
-const connectionString = { 
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: false
     }
 }
 
-const connection = new Pool(connectionString);
+const connection = new Pool(databaseConfig);
 
 export default connection;
